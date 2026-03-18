d340c63e5bf963fb94a2b224d36605c6d6d0c524
MD-789 <pedrol.campusano@gmail.com>
2026-03-18 21:22:37 +0100

    Retoque en la formula de critico

--- src/main/java/com/taller/patrones/infrastructure/strategies/CriticalDamageStrategy.java
+++ src/main/java/com/taller/patrones/infrastructure/strategies/CriticalDamageStrategy.java
@@ -6,7 +6,7 @@ import com.taller.patrones.domain.Character;
 public class CriticalDamageStrategy implements DamageStrategy{
     @Override
     public int calculateDamage(Character attacker, Character defender, Attack attack) {
-        int raw = (int) (((double) (attacker.getAttack() * attack.getBasePower()) / 100) * 1.2);
+        int raw = (int) (((double) (attacker.getAttack() * attack.getBasePower()) / 100) * 1.5);
         return Math.max(1, raw - defender.getDefense());
     }
 }
